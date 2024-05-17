b5f4b497c408751ee51d819237faaf5d9b51dfb6
isaacjermaine <isaacjermaine@hotmail.co.uk>
2024-05-17 20:08:08 +0100

    Removed model package

--- MovieRecommendation/src/main/java/org/issk/dao/UserDaoDBImpl.java
+++ MovieRecommendation/src/main/java/org/issk/dao/UserDaoDBImpl.java
@@ -1,4 +1,17 @@
 package org.issk.dao;
 
+import org.issk.dto.Genre;
+import org.issk.dto.User;
+
+import java.util.ArrayList;
+
 public class UserDaoDBImpl {
+    public UserDaoDBImpl() {
+        //Connect to DB
+    }
+
+    public User createUser(User user){
+        //Check if user is in DB
+        return null;
+    }
 }
