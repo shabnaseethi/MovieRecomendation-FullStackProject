4e476f8c395f1f9a709a6d4f4fd94f669ec39b47
Ruchita <rushah@wiley.com>
2024-05-20 15:57:48 +0100

    Created 3 API endpoints. Connected these API endpoints to 3rd party movies API.

---
+++ MovieRecommendation/src/main/java/org/issk/repository/MovieRepository.java
@@ -0,0 +1,16 @@
+package org.issk.repository;
+
+import org.issk.dto.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MovieRepository extends JpaRepository<Movie,Long> {
+
+    //List<Movie> findByGenre(String genre);
+    //List<Movie> findByRating(int rating);
+    //List<Movie> findByPreferences(String preferences);
+
+}
---
+++ MovieRecommendation/src/main/java/org/issk/service/MovieServiceImpl.java
@@ -0,0 +1,53 @@
+package org.issk.service;
+
+import org.issk.dao.MovieAPIDao;
+import org.issk.dao.MovieAPIDaoAPIImpl;
+import org.issk.dao.MovieDaoDBImpl;
+import org.issk.dto.Movie;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class MovieServiceImpl implements MovieService{
+
+    private final MovieAPIDao movieAPIDao;
+    //private final MovieDaoDBImpl movieDaoDBImpl;
+
+    @Autowired
+    public MovieServiceImpl(){
+        RestTemplate temp = new RestTemplate();
+        this.movieAPIDao= new MovieAPIDaoAPIImpl(temp);
+        //this.movieDaoDBImpl = movieDaoDBImpl;
+    }
+
+    @Override
+    public List<Movie> getMoviesByGenre(String genre) {
+        List<Movie> movies = movieAPIDao.getMoviesByGenre(genre);
+        //movieDaoDBImpl.addMoviesFromAPI(movies);
+        //movieAPIDao.getMoviesByGenre(genre);
+
+        return movies;
+    }
+
+    @Override
+    public List<Movie> getMoviesByRating(String rating, boolean sort) {
+        List<Movie> movies = movieAPIDao.getMoviesByRating(rating, sort);
+        //movieDaoDBImpl.addMoviesFromAPI(movies);
+        //movieAPIDao.getMoviesByGenre(genre);
+
+        return movies;
+    }
+
+    @Override
+    public List<Movie> getMoviesByPreferences(String userid) {
+        List<Movie> movies = movieAPIDao.getMoviesByPreferences(userid);
+        //movieDaoDBImpl.addMoviesFromAPI(movies);
+        //movieAPIDao.getMoviesByGenre(genre);
+
+        return movies;
+    }
+}
