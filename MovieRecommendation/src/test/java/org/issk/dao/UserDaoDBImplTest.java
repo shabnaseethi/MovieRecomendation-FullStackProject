b3dc57fba3775ec51ccbcbdf6f47d0ba28060929
isaacjermaine <isaacjermaine@hotmail.co.uk>
2024-05-21 11:44:27 +0100

    Store session unit tests written

--- MovieRecommendation/src/test/java/org/issk/dao/UserDaoDBImplTest.java
+++ MovieRecommendation/src/test/java/org/issk/dao/UserDaoDBImplTest.java
@@ -49,7 +49,77 @@ class UserDaoDBImplTest {
     }
 
     @Test
-    void storeSession() {
+    void createUserInvalid() {
+        User testUser = new User();
+        testUser.setUsername("Ephemeral John");
+        testUser.setPassword("test");
+
+        User newTestUser = new User();
+        newTestUser.setUsername("Ephemeral John");
+        newTestUser.setPassword("test");
+
+        boolean created = false;
+        boolean newCreated = false;
+        try { created = userDao.createUser(testUser); } catch (NoSuchAlgorithmException e) {}
+        try { newCreated = userDao.createUser(newTestUser); } catch (NoSuchAlgorithmException e) {}
+
+        assertTrue(created, "User should be created");
+        assertFalse(newCreated, "New user with same name should not be added");
+    }
+
+    @Test
+    void storeValidSession() {
+        //this user exists in the database, id: 3
+        User testUser = new User();
+        testUser.setUserId(3);
+        testUser.setUsername("person2");
+
+        Session testSession = new Session();
+        testSession.setSessionId("testSessionId");
+        testSession.setUser(testUser);
+        testSession.setStartTime(LocalDateTime.MIN);
+        testSession.setPeriodHours(1);
+
+        //Store session
+        try { userDao.storeSession(testSession); } catch (NoSuchAlgorithmException e) {}
+
+        //Fetch session
+        Session outSession = userDao.getSessionById("testSessionId");
+
+        //Assert
+        assertNotNull(outSession, "Session should be found");
+        assertEquals(outSession.getSessionId(), testSession.getSessionId(), "Session ID should match");
+
+    }
+
+    @Test
+    void storeInvalidSession() {
+        //This user exists in the database, id: 3
+        User testUser = new User();
+        testUser.setUserId(3);
+        testUser.setUsername("person2");
+
+        Session testSession = new Session();
+        testSession.setSessionId("testSessionId");
+        testSession.setUser(testUser);
+        testSession.setStartTime(LocalDateTime.MIN);
+        testSession.setPeriodHours(1);
+
+        boolean duplicateStored = false;
+
+        //Store session
+        try { userDao.storeSession(testSession); } catch (NoSuchAlgorithmException e) {}
+        try { duplicateStored = userDao.storeSession(testSession); } catch (NoSuchAlgorithmException e) {}
+
+        //Fetch session
+        Session outSession = userDao.getSessionById("testSessionId");
+
+        //Assert
+        assertNotNull(outSession, "Session should be found");
+        assertEquals(outSession.getSessionId(), testSession.getSessionId(), "Session ID should match");
+
+        assertFalse(duplicateStored, "The duplicate session shouldn't be stored");
+
     }
 
     @Test
